6e9062b71bbba3d49e328684997fab741d13a56d
benrgross <benrgross@gmail.com>
2024-06-06 16:33:13 -0400

    Commit missing files"

--- wp-content/plugins/classic-editor/js/block-editor-plugin.js
+++ wp-content/plugins/classic-editor/js/block-editor-plugin.js
@@ -1,23 +1,28 @@
-( function( wp ) {
-	if ( ! wp ) {
-		return;
-	}
+(function (wp) {
+  if (!wp) {
+    return;
+  }
 
-	wp.plugins.registerPlugin( 'classic-editor-plugin', {
-		render: function() {
-			var createElement = wp.element.createElement;
-			var PluginMoreMenuItem = wp.editPost.PluginMoreMenuItem;
-			var url = wp.url.addQueryArgs( document.location.href, { 'classic-editor': '', 'classic-editor__forget': '' } );
-			var linkText = lodash.get( window, [ 'classicEditorPluginL10n', 'linkText' ] ) || 'Switch to classic editor';
+  wp.plugins.registerPlugin('classic-editor-plugin', {
+    render: function () {
+      var createElement = wp.element.createElement;
+      var PluginMoreMenuItem = wp.editPost.PluginMoreMenuItem;
+      var url = wp.url.addQueryArgs(document.location.href, {
+        'classic-editor': '',
+        'classic-editor__forget': '',
+      });
+      var linkText =
+        lodash.get(window, ['classicEditorPluginL10n', 'linkText']) ||
+        'Switch to classic editor';
 
-			return createElement(
-				PluginMoreMenuItem,
-				{
-					icon: 'editor-kitchensink',
-					href: url,
-				},
-				linkText
-			);
-		},
-	} );
-} )( window.wp );
+      return createElement(
+        PluginMoreMenuItem,
+        {
+          icon: 'editor-kitchensink',
+          href: url,
+        },
+        linkText
+      );
+    },
+  });
+})(window.wp);
